7c4bafde2b4630625c1c99f990168fba9a8cbddd
dkz97 <r2cryptocn@gmail.com>
2022-08-27 09:12:11 +0800

    Create the Springboot project

---
+++ src/main/java/org/dkz97/sbk/SBkApplication.java
@@ -0,0 +1,13 @@
+package org.dkz97.sbk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SBkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SBkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/dkz97/sbk/SBkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dkz97.sbk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SBkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
