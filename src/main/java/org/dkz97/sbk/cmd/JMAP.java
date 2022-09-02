1c8548335a8f4d9ff9a950043f67adf1b0d8f50a
dkz97 <r2cryptocn@gmail.com>
2022-09-02 17:32:00 +0800

    add jmap execute

---
+++ src/main/java/org/dkz97/sbk/cmd/JMAP.java
@@ -0,0 +1,65 @@
+package org.dkz97.sbk.cmd;
+
+
+import cn.hutool.core.util.RuntimeUtil;
+import org.dkz97.sbk.common.DateFormatTemp;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+/**
+ * Desc: 将堆快照dump到项目目录的 /dump文件夹下
+ *
+ * @author dkz97
+ * @version 1.0.0
+ * @date 2022/9/2 16:02
+ */
+public class JMAP {
+
+    /**
+     * 返回内存占用直方图 (只有存活对象)
+     * @param id 堆快照的id
+     * @return 内存占用直方图
+     */
+    public static List<String> histoLive(String id) {
+        return RuntimeUtil.execForLines("jmap -histo:live " + id);
+    }
+
+    /**
+     * 导出堆快照，默认将堆快照下载到项目目录的 /dump文件夹下
+     * @param id 堆快照id
+     * @return 堆快照文件路径
+     * @throws IOException IOException
+     */
+    public static String dlHeapDump(String id) throws IOException {
+        String folderPath = System.getProperty("user.dir") + "/dump/";
+
+        return dlHeapDumpByDirPath(id, folderPath);
+    }
+
+    /**
+     * 导出堆快照，将堆快照下载到传入的文件夹路径下
+     * @param id 进程id
+     * @param dirPath 文件夹路径
+     * @return 堆快照文件路径
+     * @throws IOException IOException
+     */
+    public static String dlHeapDumpByDirPath(String id, String dirPath) throws IOException {
+        // 判断路径是否存在
+        Path path = Paths.get(dirPath);
+        if (!Files.exists(path)) {
+            Files.createDirectories(path);
+        }
+        String dateStr = LocalDateTime.now().format(DateTimeFormatter.ofPattern(DateFormatTemp.YYYYMMDDHHMMSS));
+        String dumpFilePath = dirPath + dateStr + "_" + id + "_heap.hprof ";
+        RuntimeUtil.exec("jmap -dump:format=b,file=" + dumpFilePath + id);
+        return dumpFilePath;
+    }
+
+
+}
---
+++ src/main/java/org/dkz97/sbk/common/DateFormatTemp.java
@@ -0,0 +1,27 @@
+package org.dkz97.sbk.common;
+
+/**
+ * Desc:
+ *
+ * @author zhangjiajun
+ * @version 1.0.0
+ * @date 2022/9/2 16:34
+ */
+public class DateFormatTemp {
+
+    /**
+     * 转换格式1：通用转换格式：yyyy-MM-dd HH:mm:ss
+     */
+    public static final String YYYY_MM_DD_HH_MM_SS= "yyyy-MM-dd HH:mm:ss";
+
+    /**
+     * 转换格式2：年-月-日
+     */
+    public static final String YYYY_MM_DD = "yyyy-MM-dd";
+
+    /**
+     *  转换格式3：年月日时分秒
+     */
+    public static final String YYYYMMDDHHMMSS = "yyyyMMddHHmmss";
+
+}
